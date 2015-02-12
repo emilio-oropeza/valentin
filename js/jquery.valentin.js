85e0d1dcb39c8722dccd34438e1d34224e4bf6ca
Vocho <vocho@MacBook-Pro-de-Emilio.local>
2015-02-11 22:20:53 -0600

    beta2
    beta2

--- js/jquery.valentin.js
+++ js/jquery.valentin.js
@@ -1,98 +1,122 @@
+(function($){
+	$.fn.quiz = function(){
+		return this.each(function() {
+			var element = $(this);						
+			if (element.data('quiz')) return;
+			var myplugin = new quiz(this);
+			element.data('quiz', myplugin);
+			element.data('quiz').methods.init();
+			
+		});
+	};
+	
+	var quiz = function(target){
+		var componentObj = {
+			index: 1,
+			cards: ["-1","-0px","-296px","-592px","-888px","-1184px",
+			"-1480px","-1776px","-2072px","-2368px",
+			"-2664px","-2960px","-3256px","-3552px",
+			"-3848px","-4144px","-4440px"],
+			methods:{
+				init:function(){		
+					console.log("hola");			
+					componentObj.methods.resize();
+					$(window).resize(function(){
+						componentObj.methods.resize();
+					});
+					$("div.indepth_choose").each(function(){
+						$(this).on("click", function(){
+							componentObj.methods.show(this);
+						});
+					});
+					$("div.indepth_cards").each(function(){
+						$(this).on("click", function(){
+							componentObj.methods.show(this);
+						});
+					});
+					$("#indepth_arrow_rigth").on("click", function(){
+						if(componentObj.index < 16){
+							componentObj.index++;
+							$("div#indepth_slider_imgs").css({"left":componentObj.cards[componentObj.index]});
+							console.log(componentObj.index);
+							if(componentObj.index == 16){
+								$("#indepth_arrow_rigth").hide();
+							}else if(componentObj.index == 2) {
+								$("#indepth_arrow_left").show();
+							}
+						}
+					});
+					$("#indepth_arrow_left").on("click", function(){
+						if(componentObj.index > 1){
+							componentObj.index--;
+							$("div#indepth_slider_imgs").css({"left":componentObj.cards[componentObj.index]});
+						}
+						if(componentObj.index == 1){
+							$("#indepth_arrow_left").hide();
+						}else if(componentObj.index == 15) {
+							$("#indepth_arrow_rigth").show();
+						}
+					});
+					$("#indepth_share_close").on("click", function(){
+						$(this).on("click", function(){
+							$(".indepth_overlay").each(function(){
+								$(this).hide();
+							});
+						});
+					});
+					$("#indepth_face_btn").on("click", function(){
+						componentObj.methods.share_facebook();
+					});
+					$("#indepth_tweet_btn").on("click", function(){
+						componentObj.methods.share_twitter();
+					});
+				},
+				show:function(id){
+					componentObj.index = parseInt($(id).parent().attr("index"));
+					$(".indepth_overlay").each(function(){
+						$(this).show();
+					});
+					$("div#indepth_slider_imgs").css({"left":componentObj.cards[componentObj.index]});
+					if(componentObj.index == 1){
+						$("#indepth_arrow_left").hide();
+						$("#indepth_arrow_rigth").show();
+					}else if(componentObj.index == 16){
+						$("#indepth_arrow_left").show();
+						$("#indepth_arrow_rigth").hide();
+					}else{
+						$("#indepth_arrow_rigth").show();
+						$("#indepth_arrow_left").show();
+					}
+				},
 
-var cards = ["-1","-0px","-296px","-592px","-888px","-1183px",
-			"-1479px","-1775px","-2071px","-2367px",
-			"-2662px","-2958px","-3254px","-3550px",
-			"-3846px","-4142px","-4438px"];
+				share_twitter:function(){
+					var text = encodeURIComponent("#JuanValentin");
+					var url = location.href;
+					var parts = url.split("?");
+					parts = parts[0].split("#");
+					url = parts[0];
+					
+					url = encodeURIComponent(url+"?cart="+componentObj.index+"&image="+urlIndepth + 'images/'+componentObj.index+'.jpg');
+					window.open("https://twitter.com/share?via=juanfutbol&text="+text+"&url="+url,"","width=500, height=300");
+				},
+				share_facebook:function(){
+					var url = location.href;
+					var parts = url.split("?");
+					parts = parts[0].split("#");
+					url = parts[0];
+					
+					url = encodeURIComponent(url+"?cart="+componentObj.index+"&image="+urlIndepth + 'images/'+componentObj.index+'.jpg');
+					window.open("https://www.facebook.com/sharer/sharer.php?u="+url,"","width=500, height=300");
+				},
+				resize:function(){
+					$("#indepth_top").height($(window).height());	
+				}
+			}
+		};
+		return componentObj;
+	};	
+})(jQuery);
 
 $(document).ready(function(){
-	resize();
-	$(window).resize(function(){
-		resize();
-	});
-
-	var index = 1;
-	$("div.indepth_choose").each(function(){
-		$(this).on("click", function(){
-			show(this);
-		});
-	});
-	$("div.indepth_cards").each(function(){
-		$(this).on("click", function(){
-			show(this);
-		});
-	});
-	$("#indepth_arrow_rigth").on("click", function(){
-		if(index < 16){
-			index += 1;
-			$("div#indepth_slider_imgs").css({"left":cards[index]});
-			if(index == 16){
-				$("#indepth_arrow_rigth").hide();
-			}else if(index == 2) {
-				$("#indepth_arrow_left").show();
-			}
-		}
-		console.log(cards[index]);
-	});
-	$("#indepth_arrow_left").on("click", function(){
-		if(index > 1){
-			index -= 1;
-			$("div#indepth_slider_imgs").css({"left":cards[index]});
-		}
-		if(index == 1){
-			$("#indepth_arrow_left").hide();
-		}else if(index == 15) {
-			$("#indepth_arrow_rigth").show();
-		}
-	});
-	$("#indepth_share_close").on("click", function(){
-		$(this).on("click", function(){
-			$(".indepth_overlay").each(function(){
-				$(this).hide();
-			});
-		});
-	});
-	$("#indepth_face_btn").on("click", function(){
-		share_facebook();
-	});
-	$("#indepth_tweet_btn").on("click", function(){
-		share_twitter();
-	});
-
+	$("#indepth_content").quiz();
 });
-
-function show(id){
-	index = $(id).parent().attr("index");
-	$(".indepth_overlay").each(function(){
-		$(this).show();
-	});
-	$("div#indepth_slider_imgs").css({"left":cards[index]});
-	if(index == 1){
-		$("#indepth_arrow_left").hide();
-	}else if(index == 16){
-		$("#indepth_arrow_rigth").hide();
-	}
-}
-
-function share_twitter(){
-	var text = encodeURIComponent("#JuanValentin");
-	var url = location.href;
-	var parts = url.split("?");
-	parts = parts[0].split("#");
-	url = parts[0];
-	
-	url = encodeURIComponent(url+"?cart="+index+"&image="+urlIndepth + 'images/'+index+'.jpg');
-	window.open("https://twitter.com/share?via=juanfutbol&text="+text+"&url="+url,"","width=500, height=300");
-}
-function share_facebook(){
-	var url = location.href;
-	var parts = url.split("?");
-	parts = parts[0].split("#");
-	url = parts[0];
-	
-	url = encodeURIComponent(url+"?cart="+index+"&image="+urlIndepth + 'images/'+index+'.jpg');
-	window.open("https://www.facebook.com/sharer/sharer.php?u="+url,"","width=500, height=300");
-}
-function resize(){
-	$("#indepth_top").height($(window).height());
-		
-}
