5e54a946a36ee55ee081cf8bd1a991e935c7a4fd
Vocho <vocho@underdog.gs>
2015-02-13 08:46:36 -0600

    Final
    Final

--- js/jquery.valentin.js
+++ js/jquery.valentin.js
@@ -17,12 +17,19 @@
 			"-1480px","-1776px","-2072px","-2368px",
 			"-2664px","-2960px","-3256px","-3552px",
 			"-3848px","-4144px","-4440px"],
+			links: ["","http://bit.ly/1ysfnPA","http://bit.ly/1E5nEh4","http://bit.ly/16ZOImG","http://bit.ly/1ysgoqM","http://bit.ly/16ZOWdy",
+			"http://bit.ly/1D0KJnV","http://bit.ly/1uKRiIo","http://bit.ly/1E5o8DI","http://bit.ly/1D0Lhdz",
+			"http://bit.ly/1MhgwUw","http://bit.ly/1KPmI4g","http://bit.ly/1F3bb15","http://bit.ly/19d4MTK",
+			"http://bit.ly/1votGnQ","http://bit.ly/1zyX8Z2","http://bit.ly/1Ck4rs1"],
 			methods:{
 				init:function(){		
 					console.log("hola");			
 					componentObj.methods.resize();
 					$(window).resize(function(){
 						componentObj.methods.resize();
+					});
+					$("div#indepth_slider_imgs img").each(function(key, item){
+						$(item).css("left", parseInt(componentObj.cards[(key+1)], 10)*-1);
 					});
 					$("div.indepth_choose").each(function(){
 						$(this).on("click", function(){
@@ -62,6 +69,7 @@
 							$(".indepth_overlay").each(function(){
 								$(this).hide();
 							});
+							$("body").css("overflow", "auto");
 						});
 					});
 					$("#indepth_face_btn").on("click", function(){
@@ -70,9 +78,15 @@
 					$("#indepth_tweet_btn").on("click", function(){
 						componentObj.methods.share_twitter();
 					});
+
+					var openItem = componentObj.methods.get_url_parameter("cart");
+					if(openItem != null){
+						$("div.indepth_choose:eq(" + (openItem-1) + ")").trigger("click");
+					}
 				},
 				show:function(id){
 					componentObj.index = parseInt($(id).parent().attr("index"));
+					$("body").css("overflow", "hidden");
 					$(".indepth_overlay").each(function(){
 						$(this).show();
 					});
@@ -88,15 +102,29 @@
 						$("#indepth_arrow_left").show();
 					}
 				},
-
+				get_url_parameter:function(sParam){
+					var sPageURL = window.location.search.substring(1);
+					var sURLVariables = sPageURL.split('&');
+					for (var i = 0; i < sURLVariables.length; i++){
+						var sParameterName = sURLVariables[i].split('=');
+						if (sParameterName[0] == sParam){
+							return sParameterName[1];
+						}
+					}
+					return null;
+				},
 				share_twitter:function(){
 					var text = encodeURIComponent("#JuanValentin");
 					var url = location.href;
 					var parts = url.split("?");
 					parts = parts[0].split("#");
 					url = parts[0];
+					if(componentObj.links[componentObj.index] != undefined){
+						url = componentObj.links[componentObj.index];
+					}else{
+						url = encodeURIComponent(url+"?cart="+componentObj.index+"&image="+urlIndepth + 'images/'+componentObj.index+'.jpg');
+					}
 					
-					url = encodeURIComponent(url+"?cart="+componentObj.index+"&image="+urlIndepth + 'images/'+componentObj.index+'.jpg');
 					window.open("https://twitter.com/share?via=juanfutbol&text="+text+"&url="+url,"","width=500, height=300");
 				},
 				share_facebook:function(){
@@ -104,8 +132,12 @@
 					var parts = url.split("?");
 					parts = parts[0].split("#");
 					url = parts[0];
+					if(componentObj.links[componentObj.index] != undefined){
+						url = componentObj.links[componentObj.index];
+					}else{
+						url = encodeURIComponent(url+"?cart="+componentObj.index+"&image="+urlIndepth + 'images/'+componentObj.index+'.jpg');
+					}
 					
-					url = encodeURIComponent(url+"?cart="+componentObj.index+"&image="+urlIndepth + 'images/'+componentObj.index+'.jpg');
 					window.open("https://www.facebook.com/sharer/sharer.php?u="+url,"","width=500, height=300");
 				},
 				resize:function(){
